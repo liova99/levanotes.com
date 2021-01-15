51091df39b13e6e985084e9946d4fc392e0ece90
Levan Kelesidis <levan.kelesidis@gmail.com>
2021-01-15 08:23:04 +0100

    Gulp for scripts

--- LK/gulpfile.js
+++ LK/gulpfile.js
@@ -1,21 +1,65 @@
-﻿const gulp = require('gulp');
-const sass = require('gulp-sass');
-const sourceMaps = require('gulp-sourcemaps');
-const watch = require('gulp-watch');
-const gulpSrc = './wwwroot/scss/**/*.scss';
+﻿const
+    gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    uglify = require('gulp-uglify'),
+    cssMin = require('gulp-minify-css')
+    sourceMaps = require('gulp-sourcemaps'),
+    watch = require('gulp-watch'),
+    rename = require('gulp-rename'),
+    concat = require('gulp-concat'),
+    filter = require('gulp-filter'),
 
-// you can choose a name ex gulp.task('sdfasdfasd')
+    rootPath = './wwwroot',
+    srcScss = './wwwroot/scss/**/*.scss';
+
+var libScripts = [
+    // highlight.min.js', will be added in _Post.cshtml for performance reasons
+    'wwwroot/lib/jquery/dist/jquery.min.js',
+    'wwwroot/lib/bootstrap/dist/js/bootstrap.bundle.min.js',
+]
+
+var siteScripts = [
+    'wwwroot/js/site.js'
+]
+
+
+gulp.task('scripts', async function () {
+
+    var scripts = [];
+    scripts = scripts.concat(libScripts, siteScripts);
+
+    const filterMin = filter(['!*min.js'], { restore: true });
+
+    return gulp
+        .src(scripts)
+        .pipe(sourceMaps.init())
+        .pipe(filterMin)
+        .pipe(uglify())
+        .pipe(filterMin.restore)
+        .pipe(concat('site.js'))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(sourceMaps.write('./'))
+        .pipe(gulp.dest('./wwwroot/js/'))
+});
+
+
 gulp.task('sass-compile', function () {
-    // in root dir -> wwwwroot -> scss -> "a" file (** means we don't specify a file name)
+    // in root dir -> wwwwroot -> scss -> "afile.scss" (** means we don't specify a file name)
     //-> .* scss(all files with .scss extension)
-    return gulp.src(gulpSrc)
-        // conect the moduls
+    return gulp
+        .src(srcScss)
         .pipe(sourceMaps.init())
         .pipe(sass().on('error', sass.logError))
+        .pipe(cssMin({ keepBreaks: false }))
+        .pipe(rename({ suffix: '.min' }))
         .pipe(sourceMaps.write('./'))
         .pipe(gulp.dest('./wwwroot/css/'))
 });
 
 gulp.task('watch', function () {
-    gulp.watch(gulpSrc, gulp.series('sass-compile'))
+    const srcJs = './wwwroot/js/**/*.js';
+    const filterJs = '!./wwwroot/js/**/*min.js';
+
+    gulp.watch(srcScss, gulp.series('sass-compile'));
+    gulp.watch([srcJs, filterJs ], gulp.series('scripts'));
 });
