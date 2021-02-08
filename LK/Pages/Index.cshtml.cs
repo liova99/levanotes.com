7cf02613eba7da08f06e530361064106b6954c8c
Levan Kelesidis <levan.kelesidis@gmail.com>
2021-02-08 09:49:40 +0100

    * removed index page in Blog directory. * Moved Latest posts to the startpage * Created a partial page for the post preview

--- LK/Pages/Index.cshtml.cs
+++ LK/Pages/Index.cshtml.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LK.DB.Models;
+using LK.DB.Repos;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using System;
@@ -11,15 +13,18 @@ namespace LK.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly IPostRepo _postRepo;
+        public IEnumerable<Post> Posts { get; set; }
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, IPostRepo postRepo)
         {
             _logger = logger;
+            _postRepo = postRepo;
         }
 
         public void OnGet()
         {
-
+            Posts = _postRepo.GetAllPosts();
         }
     }
 }
