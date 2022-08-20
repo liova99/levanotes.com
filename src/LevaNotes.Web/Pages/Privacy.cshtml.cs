543faaa5010c09bfda9c930ca1f0340a17f65917
Levan Kelesidis <levan.kelesidis@gmail.com>
2022-08-20 16:07:39 +0200

    fixed naming omission

--- src/LevaNotes.Web/Pages/Privacy.cshtml.cs
+++ src/LevaNotes.Web/Pages/Privacy.cshtml.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace LevaNotes.Pages
+namespace LevaNotes.Web.Pages
 {
     public class PrivacyModel : PageModel
     {
