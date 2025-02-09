06e6b63127f6ffb8b4fc89d7ff561e287126153b
Levan Kelesidis <levan.kelesidis@gmail.com>
2025-02-09 11:14:10 +0100

    to top button

--- src/LevaNotes.Web/wwwroot/js/site.js
+++ src/LevaNotes.Web/wwwroot/js/site.js
@@ -1,5 +1,23 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-var te = 'a';
+let toTopButton = document.getElementById("toTopBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        /*toTopButton.style.display = "block";*/
+        toTopButton.classList.add("showBtn")
+    } else {
+        //toTopButton.style.display = "none";
+        toTopButton.classList.remove("showBtn")
+    }
+}
+
+function scrollToTop() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For normal browsers
+}
